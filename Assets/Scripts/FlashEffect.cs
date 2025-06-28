0684464d041a59e7cd4d9810d10b35388a7daee2
Roy ten Brink <tenBrinkRoy+deere@JohnDeere.com>
2025-06-28 07:01:43 -0500

    juicification

---
+++ Assets/Scripts/FlashEffect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class FlashEffect : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Material originalMaterial;
+    [SerializeField] private Material flashMaterial;
+    [SerializeField] private float flashDuration = 0.4f;
+    private float flashTimeElapsed = Mathf.Infinity;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        originalMaterial = sr.material;
+    }
+
+    private void Update()
+    {
+        flashTimeElapsed += Time.deltaTime;
+
+        if (flashTimeElapsed > flashDuration && sr.material != originalMaterial)
+        {
+            sr.material = originalMaterial;
+        }
+    }
+
+    public void Activate()
+    {
+        print("activated");
+        flashTimeElapsed = 0;
+        sr.material = flashMaterial;
+    }
+
+
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,13 +2,13 @@ using System;
 using Unity.Mathematics;
 using UnityEngine;
 
-
 public class Health : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 10;
     private int currentHealth;
     public Action<int> healthUpdated;
     public Action hasDied;
+    
     private void Start()
     {
         currentHealth = maxHealth;
@@ -23,9 +23,15 @@ public class Health : MonoBehaviour
         {
             Die();
         }
-
+        
         healthUpdated?.Invoke(currentHealth);
 
+
+        var flashEffect = GetComponent<FlashEffect>();
+        if (flashEffect != null)
+        {
+            flashEffect.Activate();
+        }
     }
 
     private void Die()
